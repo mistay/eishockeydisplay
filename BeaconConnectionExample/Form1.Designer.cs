24935524b3bb62dcfbe6d561ca43b0954d666fb4
armin <office@langhofer.at>
2017-03-02 15:14:12 +0100

    added version number v0.3, release

--- BeaconConnectionExample/Form1.Designer.cs
+++ BeaconConnectionExample/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.txtData = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.lblLogPath = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -165,11 +166,21 @@
             this.lblLogPath.Text = "lblLogPath";
             this.lblLogPath.Click += new System.EventHandler(this.label2_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 319);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "v0.3";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(881, 326);
+            this.ClientSize = new System.Drawing.Size(881, 345);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.lblLogPath);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtData);
@@ -215,6 +226,7 @@
         private System.Windows.Forms.TextBox txtData;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label lblLogPath;
+        private System.Windows.Forms.Label label2;
     }
 }
 
